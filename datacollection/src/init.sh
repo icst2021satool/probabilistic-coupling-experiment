a7f3239713d61885c765842eb86cdb67e4e66e36
icst2021satool <icst2021satool@gmail.com>
2022-06-14 14:33:29 -0300

    Added env var DATACOLLECTION_HOME

--- datacollection/src/init.sh
+++ datacollection/src/init.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 #set -x
 # Set the D4J_HOME environment var
+# It's set with the docker directory, but you may set with your own D4J directory
+
 export D4J_HOME="/stvr/defects4j"
+
+# Set the DATACOLLECTION_HOME environment var
+# It's set with the docker directory, but you may set with your own D4J directory
+
+export DATACOLLECTION_HOME="/stvr/probablistic_coupling_repo/datacollection"
