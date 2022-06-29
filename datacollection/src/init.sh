7a4bc2701ed2f4ae7f8eaaa0298a2a8df0917081
icst2021satool <icst2021satool@gmail.com>
2022-06-29 22:58:51 +0000

    Ajustment to synch

--- datacollection/src/init.sh
+++ datacollection/src/init.sh
@@ -8,4 +8,4 @@ export D4J_HOME="/stvr/defects4j"
 # Set the DATACOLLECTION_HOME environment var
 # It's set with the docker directory, but you may set with your own D4J directory
 
-export DATACOLLECTION_HOME="/stvr/probablistic_coupling_repo/datacollection"
+export DATACOLLECTION_HOME="/stvr/probabilistic-coupling-experiment/datacollection"
