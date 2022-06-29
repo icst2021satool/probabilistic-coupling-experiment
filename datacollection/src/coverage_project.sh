a3dbcc50d2e8416b4ed70239a0f3531b4a7bbd51
icst2021satool <icst2021satool@gmail.com>
2022-06-29 22:57:37 +0000

    Fixed to run in batch mode

--- datacollection/src/coverage_project.sh
+++ datacollection/src/coverage_project.sh
@@ -7,6 +7,7 @@ readonly YELLOW="\033[1;33m"
 
 readonly PROJECT_NAME=$1
 readonly VERSION=$2
+readonly ZIPFILE=$3
 
 function assert_project() {
 	local root=$(pwd)
@@ -38,8 +39,59 @@ function assert_project() {
 	cd $root
 }
 
+function coverage_expansion() {
+local root=$1
+local coveragedir="$1/coverage/$2"
+local project_dir=$3
+local zipfile=$ZIPFILE # program_dir
+
+if [ ! -f "$coveragedir/$zipfile" ]
+then
+  echo "$coveragedir/$zipfile does not exist. Try to get it remotely"
+  mkdir -p $coveragedir
+  cd $coveragedir
+  wget http://143.107.58.177/${zipfile}
+
+  if [ ! -f "$coveragedir/$zipfile" ]
+  then
+  echo "$coveragedir/$zipfile does not exit. Couldn't get it remotely. End of story."
+  exit
+  fi
+fi
+
+cd $coveragedir
+
+local filesdir="$project_dir/$VERSION/*"
+rm -rf $filesdir
+
+if [ -d "$project_dir/$VERSION" ]
+then
+ rmdir "$project_dir/$VERSION"
+fi
+
+unzip $zipfile $filesdir
+cd $root
+}
+
+function cleanup() {
+  #statements
+  local root=$1
+  local coveragedir="$1/coverage/$PROJECT_NAME"
+  local project_dir=$2
+  local covfilesdir="$project_dir/$VERSION"
+
+  cd $coveragedir
+
+  if [ -d "$covfilesdir" ]
+  then
+    rm -rf $covfilesdir
+  fi
+  cd $root
+}
+
 function main() {
-  pwd
+  	pwd
+	local root=$(pwd)
 	if [ -z "$PROJECT_NAME" ]
 	then
 	    echo "Missing project identifier. Possible values are:"
@@ -65,7 +117,7 @@ function main() {
 		echo
 		exit 1
 	fi
-
+	coverage_expansion $root $PROJECT_NAME $project_dir $ZIPFILE
 	assert_project $project_dir
 }
 
