164b1af1073b890bea66e0ab34c7662812295f75
icst2021satool <icst2021satool@gmail.com>
2022-07-02 21:16:10 +0000

    Cleanup of versions with no probabalistic coupling data for Chart and Lang

--- datacollection/batch-src/cleanup_cft_comparison.sh
+++ datacollection/batch-src/cleanup_cft_comparison.sh
@@ -11,7 +11,7 @@ Identifiers="Chart Cli Closure Codec Collections Compress Csv Gson JacksonCore J
 
 echo "Remove unreliable versions data"
 
-blockChart="Chart-8b Chart-10b"
+blockChart="Chart-6b Chart-8b Chart-10b" # Chart-6b is added to be cleaned up because in the uncontrained duas vs. duas comparison it is cleaned up due to the fact that the fdp is 0.0 for both uncontrained duas and duas.
 
 blockCli="Cli-6b"
 
@@ -28,7 +28,7 @@ blockJacksonCore="JacksonCore-5b JacksonCore-19b JacksonCore-23b JacksonCore-25b
 
 blockJacksonDatabind="JacksonDatabind;44b JacksonDatabind;45b JacksonDatabind-95b"
 
-blockLang="Lang;2b Lang;6b Lang;11b Lang;25b Lang;35b Lang;36b Lang;37b Lang;38b Lang;39b Lang;40b Lang;41b Lang;42b Lang;43b Lang;44b Lang;45b Lang;46b Lang;47b Lang;48b Lang;49b Lang;50b Lang;51b Lang;52b Lang;53b Lang;54b Lang;55b Lang;56; Lang;57b Lang;58b Lang;59b Lang;60b Lang;61b Lang;62b Lang;63b Lang;64b"
+blockLang="Lang-2b Lang-6b Lang-11b Lang-25b Lang-35b Lang-36b Lang-37b Lang-38b Lang-39b Lang-40b Lang-41b Lang-42b Lang-43b Lang-44b Lang-45b Lang-46b Lang-47b Lang-48b Lang-49b Lang-50b Lang-51b Lang-52b Lang-53b Lang-54b Lang-55b Lang-56b Lang-57b Lang-58b Lang-59b Lang-60b Lang-61b Lang-62b Lang-63b Lang-64b"
 
 blockMath="Math-3b Math-55b Math-89b"
 
--- datacollection/batch-src/cleanup_udua_comparison.sh
+++ datacollection/batch-src/cleanup_udua_comparison.sh
@@ -44,7 +44,7 @@ blockJacksonCore="JacksonCore;5b JacksonCore;19b JacksonCore;23b JacksonCore;25b
 
 blockJacksonDatabind="JacksonDatabind;44b JacksonDatabind;45b JacksonDatabind;95b"
 
-blockLang="Lang;2b Lang;6b Lang;11b Lang;25b Lang;35b Lang;36b Lang;37b Lang;38b Lang;39b Lang;40b Lang;41b Lang;42b Lang;43b Lang;44b Lang;45b Lang;46b Lang;47b Lang;48b Lang;49b Lang;50b Lang;51b Lang;52b Lang;53b Lang;54b Lang;55b Lang;56; Lang;57b Lang;58b Lang;59b Lang;60b Lang;61b Lang;62b Lang;63b Lang;64b"
+blockLang="Lang;2b Lang;6b Lang;11b Lang;25b Lang;35b Lang;36b Lang;37b Lang;38b Lang;39b Lang;40b Lang;41b Lang;42b Lang;43b Lang;44b Lang;45b Lang;46b Lang;47b Lang;48b Lang;49b Lang;50b Lang;51b Lang;52b Lang;53b Lang;54b Lang;55b Lang;56b Lang;57b Lang;58b Lang;59b Lang;60b Lang;61b Lang;62b Lang;63b Lang;64b"
 
 blockMath="Math;3b Math;55b Math;89b"
 
